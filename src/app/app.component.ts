21b4b6225128a457a8c5b7ac47d2609a6c2b029a
Raul Monroy <sBlinkRst@outlook.com>
2021-02-22 13:58:00 -0600

    Add images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pika';
+  images: string[] = [];
+constructor() {
+  this.SetImages();
 }
+  SetImages(): void {
+    const dr = '/assets/img/';
+    const ext = '.png';
+    this.images.push(dr + 1 + ext);
+    this.images.push(dr + 2 + ext);
+    this.images.push(dr + 3 + ext);
+    this.images.push(dr + 4 + ext);
+    this.images.push(dr + 5 + ext);
+    this.images.push(dr + 6 + ext);
+    this.images.push(dr + 7 + ext);
+    this.images.push(dr + 8 + ext);
+    this.images.push(dr + 9 + ext);
+    }
+}
+
